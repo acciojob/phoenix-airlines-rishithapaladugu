08271b5401ce7c5347b5f12feef34df38b253055
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-10 07:43:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
 import React from "react";
-import './../styles/App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./Landing";
+import FlightSearch from "./FlightSearch";
+import FlightBooking from "./FlightBooking";
+import Confirmation from "./Confirmation";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/flight-search" component={FlightSearch} />
+        <Route exact path="/flight-booking" component={FlightBooking} />
+        <Route exact path="/confirmation" component={Confirmation} />
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
