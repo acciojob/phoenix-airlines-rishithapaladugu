2556b31e79d8bc8433547e8d9bd10fdae9ec6cd0
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-21 11:10:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
+import "./styles/App.css";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
