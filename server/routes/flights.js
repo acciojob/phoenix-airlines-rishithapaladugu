6cf1edbc3085f93dad8cea7d6257574dbd159c56
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-22 22:16:58 +0530

    Create flights.js

---
+++ server/routes/flights.js
@@ -0,0 +1,5 @@
+import express from "express";
+import { searchFlights } from "../controllers/flightController.js";
+const router = express.Router();
+router.get("/", searchFlights);
+export default router;
