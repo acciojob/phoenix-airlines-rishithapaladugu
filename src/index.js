43287e71d79ee4e2d04de5a0675bd20491e3a4d1
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-22 11:41:20 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { store } from "./store";
-import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
-import "./styles/App.css";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
