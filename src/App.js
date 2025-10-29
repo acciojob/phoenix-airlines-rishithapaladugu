2bd273a97b924183df45245db5639c39971a5abf
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-29 11:34:37 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import LandingPage from './components/LandingPage';
+import FlightSearch from './components/FlightSearch';
+import FlightBooking from './components/FlightBooking';
+import Confirmation from './components/Confirmation';
+
+function App() {
+   return (
+       <Provider store={store}>
+           <Router>
+               <Switch>
+                   <Route path="/" exact component={LandingPage} />
+                   <Route path="/flight-search" component={FlightSearch} />
+                   <Route path="/flight-booking" component={FlightBooking} />
+                   <Route path="/confirmation" component={Confirmation} />
+               </Switch>
+           </Router>
+       </Provider>
+   );
+}
+
+export default App;
