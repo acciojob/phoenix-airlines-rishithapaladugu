6adaef355b4c6cc22443d3b1930cf3290d1025c0
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-29 10:27:35 +0530

    Delete server/routes/flights.js

--- server/routes/flights.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { searchFlights } from "../controllers/flightController.js";
-const router = express.Router();
-router.get("/", searchFlights);
-export default router;
