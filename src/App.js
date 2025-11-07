627e9d503b1e897d957cb2c0c50779bf84baf1a3
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-11-07 22:16:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store from './store';
 import LandingPage from './components/LandingPage';
 import FlightSearch from './components/FlightSearch';
 import FlightBooking from './components/FlightBooking';
 import Confirmation from './components/Confirmation';
 
 function App() {
-   return (
-       <Provider store={store}>
-           <Router>
-               <Switch>
-                   <Route path="/" exact component={LandingPage} />
-                   <Route path="/flight-search" component={FlightSearch} />
-                   <Route path="/flight-booking" component={FlightBooking} />
-                   <Route path="/confirmation" component={Confirmation} />
-               </Switch>
-           </Router>
-       </Provider>
-   );
+    return (
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={LandingPage} />
+                    <Route path="/flight-search" component={FlightSearch} />
+                    <Route path="/flight-booking" component={FlightBooking} />
+                    <Route path="/confirmation" component={Confirmation} />
+                </Switch>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
