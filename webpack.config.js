395fc7b24fdadc034caf000385b9798116902b4b
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-11-07 23:03:32 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin= require('html-webpack-plugin');
-module.exports = {
-    entry: './src/index.js',
+const path = require('path');
 
-    output: {
-        path: path.join(__dirname,"/dist"),
-        filename: "index_bundle.js",
-    },
-    module:{
-        rules: [
-            {
-                test: /\.js$|\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                ]
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
+module.exports = {
+  entry: './src/index.js', // Entry point of your application
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js', // Output file name
+    publicPath: '/', // Public URL of the output directory when referenced in a browser
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Automatically resolve these extensions
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Regex to match files
+        exclude: /node_modules/, // Exclude node_modules
+        use: {
+          loader: 'babel-loader', // Use Babel to transpile JavaScript
+        },
+      },
+      {
+        test: /\.css$/, // Regex to match CSS files
+        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader for CSS files
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/, // Regex to match image files
+        use: {
+          loader: 'file-loader', // Use file-loader for images
+          options: {
+            name: '[path][name].[ext]', // Preserve original name and path
+          },
+        },
+      },
     ],
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'), // Serve content from the output directory
+    compress: true, // Enable gzip compression
+    port: 3000, // Port number
+    historyApiFallback: true, // Enable support for HTML5 History API
+  },
+  mode: 'development', // Set mode to development
 };
